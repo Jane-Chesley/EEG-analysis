64014f63f2c4a05716855393bf8dbf3329aa1db4
Jane Chesley <118343044+Jane-Chesley@users.noreply.github.com>
2024-03-11 15:11:19 +0100

    updates

--- main_scripts/FDR_correction.m
+++ main_scripts/FDR_correction.m
@@ -2,14 +2,15 @@
 clear;clc; close all;
 
 % input original p-values 
-orig_p = [0.016; 0.072;0.060; 0.110; 0.371; 0.006];
+% orig_p = [0.016; 0.072;0.060; 0.110; 0.371; 0.006];
+orig_p = [0.001;0.270;0.009];
 
 % output FDR corrected p-values 'adj_p'
-% 'pdep' (default) = independent tests; 'dep' = dependent tests 
+% 'pdep' (default) = independent tests; 'dep' = independent or dependent tests 
 % 'yes' or 'no' (default) = provide summary report to command line 
 % [h, crit_p, adj_ci_cvrg, adj_p]=fdr_bh(orig_p,0.005);
 q = 0.01;
-[h, crit_p, adj_ci_cvrg, adj_p]=fdr_bh(orig_p,q,'pdep','yes');
+[h, crit_p, adj_ci_cvrg, adj_p]=fdr_bh(orig_p,q,'dep','yes');
 
 disp(orig_p)
 disp(adj_p)
